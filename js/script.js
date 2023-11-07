ad206ddaebc112585f68cedbcddb53d74aeed2e0
Ahmed Helal <87047003+HELAL15@users.noreply.github.com>
2023-11-07 11:56:29 +0200

    profile

--- js/script.js
+++ js/script.js
@@ -53,6 +53,25 @@ $(document).ready(function () {
 });
 
 
+////////////////////// profile photo change ///////////////////////////
+
+let profile_photo = document.getElementById('profile-photo');
+let photo = document.getElementById('photo');
+let file = document.getElementById('file');
+
+if( file !=null){
+  file.onchange = ()=>{
+    let choosedFile = file.files[0];
+    if(choosedFile){
+      let reader = new FileReader();
+      reader.onload = ()=>{
+        photo.setAttribute('src' , reader.result);
+      }
+      reader.readAsDataURL(choosedFile);
+    }
+  }
+}
+
 
 // loader
 $('#loading').fadeOut(500);
