65850c4631928ec0696b802ffaec2970d671c0dc
Ahmed Helal <87047003+HELAL15@users.noreply.github.com>
2023-11-09 14:45:36 +0200

    finish project

--- js/script.js
+++ js/script.js
@@ -110,6 +110,48 @@ console.log(section.offsetTop);
 window.addEventListener('scroll', change_active);
 
 
+$(document).ready(function() {
+  $('select').niceSelect();
+});
+
+
+
+const inputElements = [...document.querySelectorAll('input.code')]
+    
+    inputElements.forEach((ele,index)=>{
+      ele.addEventListener('keydown',(e)=>{
+        // if the keycode is backspace & the current field is empty
+        // focus the input before the current. Then the event happens
+        // which will clear the "before" input box.
+        if(e.keyCode === 8 && e.target.value==='') inputElements[Math.max(0,index-1)].focus()
+      })
+      ele.addEventListener('input',(e)=>{
+          inputElements[index].focus()
+        const [first,...rest] = e.target.value
+        e.target.value = first ?? '' // first will be undefined when backspace was entered, so set the input to ""
+        const lastInputBox = index===inputElements.length-1
+        const didInsertContent = first!==undefined
+        if(didInsertContent && !lastInputBox) {
+          // continue to input the rest of the string
+          inputElements[index+1].focus()
+          inputElements[index+1].value = rest.join('')
+          inputElements[index+1].dispatchEvent(new Event('input'))
+        }
+      })
+    })
+    
+    
+    // mini example on how to pull the data on submit of the form
+    function onSubmit(e){
+      e.preventDefault()
+      const code = inputElements.map(({value})=>value).join('')
+      console.log(code)
+    }
+
+
+
+
+
 // loader
 $('#loading').fadeOut(500);
 
